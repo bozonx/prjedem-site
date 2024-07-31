d1b6478ddd35389f6716226ae3ab275ff5d1d861
Ivan <ipkozyrin@gmail.com>
2024-07-31 20:00:17 -0300

    WIP

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -17,4 +17,22 @@ export default {
     ...configBase.themeConfig,
     logo: "/img/logo.svg",
   },
+  head: [
+    // do not recognize telephone numbers on the page
+    ["meta", { name: "format-detection", content: "telephone=no" }],
+
+    [
+      "script",
+      {
+        async: true,
+        src: "https://www.googletagmanager.com/gtag/js?id=G-MNN0LKCRMQ",
+      },
+    ],
+    [
+      "script",
+      {},
+      `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);} gtag('js', new Date());
+       gtag('config', 'G-MNN0LKCRMQ');`,
+    ],
+  ],
 };
